58636a0c53a951a82772c1cfff960b311de1cde8
hsiuqnav <liuyijia.liu@gmail.com>
2016-03-01 13:24:06 +0800

    Longest Palindromic Substring

---
+++ LeetCode/Longest Palindromic Substring.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		if (s.empty())
+			return "";
+		if (s.length() == 1)
+			return s;
+		int start = 0, max_length = 0;
+		int middle = 0;
+		while (middle < s.length() && s.length() - middle > max_length / 2) {
+			int left = middle, right = middle;
+			while (right < s.length() - 1 && s[right] == s[right + 1])
+				right ++;
+			middle ++;
+			while (left > 0 && s[left - 1] == s[right + 1]) {
+				left --;
+				right ++;
+			}
+			if (max_length < right - left + 1) {
+				max_length = right - left + 1;
+				start = left;
+			}
+		}
+		return s.substr(start, max_length);
+	}
+};
+
+int main() {
+	Solution s;
+	string e = "abcdddcerd";
+	cout << s.longestPalindrome(e) << endl;
+}
